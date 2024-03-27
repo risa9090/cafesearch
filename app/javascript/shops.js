8d8d37960abfae9fbdffd26c309dace8384c286d
RISA <161457959+risa9090@users.noreply.github.com>
2024-03-27 21:37:16 +0900

    Delete app directory

--- app/javascript/shops.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $(".btn").on("click", function () {
-        $(this).closest("div").css("display", "none");
-        id = $(this).attr("href");
-        $(id).addClass("fit").show("slow");
-    });
-});
